205d848cf2918485d6232f0e638995615e2f8ae9
Nada Alaa <nadaalaa.na@gmail.com>
2016-12-12 01:20:46 +0200

    Initial logic in serial mode

--- Visualization of Temperature Flow/Mesh.cs
+++ Visualization of Temperature Flow/Mesh.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Drawing;
 namespace Visualization_of_Temperature_Flow
 {
+<<<<<<< HEAD
    public class Mesh
     {
        
@@ -57,5 +58,65 @@ namespace Visualization_of_Temperature_Flow
                    break;
            }
        }
+=======
+    public class Mesh
+    {
+        Cell[][] grid;
+        public int cellsize;
+        public CellType targetType;
+        int rows, cols;
+
+        public Mesh(int width, int height, int cellsize)
+        {
+            this.cellsize = cellsize;
+            rows = (height / cellsize) + 1;
+            cols = (width / cellsize) + 1;
+            grid = new Cell[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                grid[i] = new Cell[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    grid[i][j] = new Cell(new Point(j * cellsize, i * cellsize), Color.Green, CellType.NormalCell);
+                }
+            }
+        }
+
+        public void Update()
+        { }
+
+        public void Draw()
+        {
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    grid[i][j].Draw(cellsize);
+        }
+
+        public void ChangeCell(int row, int col)
+        {
+            switch (targetType)
+            {
+                case CellType.Block:
+                    grid[row][col] = new Cell(new Point(col * cellsize, row * cellsize), Color.Black, CellType.Block);
+                    break;
+
+                case CellType.ColdSource:
+                    grid[row][col] = new Cell(new Point(col * cellsize, row * cellsize), Color.Blue, CellType.ColdSource);
+                    break;
+
+                case CellType.HeatSource:
+                    grid[row][col] = new Cell(new Point(col * cellsize, row * cellsize), Color.Red, CellType.HeatSource);
+                    break;
+
+                case CellType.NormalCell:
+                    grid[row][col] = new Cell(new Point(col * cellsize, row * cellsize), Color.Green, CellType.NormalCell);
+                    break;
+
+                case CellType.Window:
+                    grid[row][col] = new Cell(new Point(col * cellsize, row * cellsize), Color.Yellow, CellType.Window);
+                    break;
+            }
+        }
+>>>>>>> Initial logic in serial mode
     }
 }
